9e61ce77c77fa0ef3f50d76471fe65b13984c65d
samazon <letcsam@gmail.com>
2021-11-04 12:52:09 +0500

    React Component Types

---
+++ src/components/GreetCC.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+/* GreetCC
+    Stands for Greet Class Component
+*/
+export default class GreetCC extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Class Component</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/GreetFC.js
@@ -0,0 +1,13 @@
+import React from 'react';
+/* GreetFC
+    Stands for Greet Functional Component
+*/
+const GreetFC = () => {
+  return (
+    <div>
+      <h1>Hello from Functional Component!</h1>
+    </div>
+  );
+};
+
+export default GreetFC;
